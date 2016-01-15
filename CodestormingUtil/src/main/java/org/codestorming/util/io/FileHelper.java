0d4c9f80419bccd5ed3bc4363ee7d654afae3321
Thaedrik <thaedrik@gmail.com>
2016-01-15 19:51:13 +0100

    Minor changes on the copyright headers

--- CodestormingUtil/src/main/java/org/codestorming/util/io/FileHelper.java
+++ CodestormingUtil/src/main/java/org/codestorming/util/io/FileHelper.java
@@ -1,14 +1,14 @@
-/****************************************************************************
- * Copyright (c) 2012-2014 Codestorming.org
- * 
+/*
+ * Copyright (c) 2012-2016 Codestorming.org
+ *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
- * 
+ *
  * Contributors:
  *     Codestorming - initial implementation and API
- ****************************************************************************/
+ */
 package org.codestorming.util.io;
 
 import java.io.Closeable;
