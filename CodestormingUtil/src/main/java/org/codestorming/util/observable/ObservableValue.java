fa2ac615868b39fc672d15ab29cb2cf7540d56e6
Thaedrik <thaedrik@gmail.com>
2016-03-29 19:08:01 +0200

    Added observable values API

---
+++ CodestormingUtil/src/main/java/org/codestorming/util/observable/ObservableValue.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright (c) 2012-2016 Codestorming.org
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Codestorming - initial implementation and API
+ */
+package org.codestorming.util.observable;
+
+/**
+ * {@code ObservableValue} references an object and notifies the registered {@link ChangeListener}s when the reference
+ * to the object changes.
+ *
+ * @author Thaedrik <thaedrik@codestorming.org>
+ */
+public interface ObservableValue<T> {
+
+	/**
+	 * Returns the observed value.
+	 *
+	 * @return the observed value.
+	 */
+	T get();
+
+	/**
+	 * Changes the value and notifies the registered listeners of the change.
+	 *
+	 * @param value the new value.
+	 */
+	void set(T value);
+
+	/**
+	 * Add the given {@link ChangeListener} to this {@code ObservableValue}.
+	 *
+	 * @param listener The {@link ChangeListener} to add.
+	 */
+	void addChangeListener(ChangeListener<T> listener);
+
+	/**
+	 * Remove the given {@link ChangeListener} from this {@code ObservableValue}.
+	 *
+	 * @param listener The {@link ChangeListener} to remove.
+	 */
+	void removeChangeListener(ChangeListener<T> listener);
+}
---
+++ CodestormingUtil/src/main/java/org/codestorming/util/observable/SimpleObservableValue.java
@@ -0,0 +1,110 @@
+/*
+ * Copyright (c) 2012-2016 Codestorming.org
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Codestorming - initial implementation and API
+ */
+package org.codestorming.util.observable;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
+/**
+ * Default implementation of an {@link ObservableValue}.
+ *
+ * @author Thaedrik <thaedrik@codestorming.org>
+ */
+public class SimpleObservableValue<T> implements ObservableValue<T> {
+
+	protected T value;
+
+	protected final Set<ChangeListener<T>> changeListeners = new HashSet<ChangeListener<T>>();
+
+	private final ReadWriteLock lock = new ReentrantReadWriteLock();
+
+	/**
+	 * Creates a {@code SimpleObservableValue} initialized with {@code null}.
+	 */
+	public SimpleObservableValue() {}
+
+	/**
+	 * Creates a {@code SimpleObservableValue} initialized with the given value.
+	 *
+	 * @param value The value.
+	 */
+	public SimpleObservableValue(T value) {
+		this.value = value;
+	}
+
+	@Override
+	public T get() {
+		return value;
+	}
+
+	@Override
+	public void set(T value) {
+		if (value != this.value) {
+			T oldValue = this.value;
+			this.value = value;
+			fireChange(oldValue, value);
+		}
+	}
+
+	@Override
+	public void addChangeListener(ChangeListener<T> listener) {
+		writeLock();
+		try {
+			changeListeners.add(listener);
+		} finally {
+			writeUnlock();
+		}
+	}
+
+	@Override
+	public void removeChangeListener(ChangeListener<T> listener) {
+		writeLock();
+		try {
+			changeListeners.remove(listener);
+		} finally {
+			writeUnlock();
+		}
+	}
+
+	protected void readLock() {
+		lock.readLock().lock();
+	}
+
+	protected void readUnlock() {
+		lock.readLock().unlock();
+	}
+
+	protected void writeLock() {
+		lock.writeLock().lock();
+	}
+
+	protected void writeUnlock() {
+		lock.writeLock().unlock();
+	}
+
+	protected void fireChange(T oldValue, T newValue) {
+		readLock();
+		List<ChangeListener<T>> changeListeners;
+		try {
+			changeListeners = new ArrayList<ChangeListener<T>>(this.changeListeners);
+		} finally {
+			readUnlock();
+		}
+		for (ChangeListener<T> listener : changeListeners) {
+			listener.onChange(this, oldValue, newValue);
+		}
+	}
+}
